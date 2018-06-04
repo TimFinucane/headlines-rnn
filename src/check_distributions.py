dc71d5452ad3bf849cba31013702dd5df8b7ee6d
Timothy Finucane <timfinucane@outlook.com>
2018-06-04 17:00:46 +1200

    Update distributions code

--- src/check_distributions.py
+++ src/check_distributions.py
@@ -4,28 +4,32 @@ from matplotlib import pyplot as plt
 
 distribution = defaultdict(int)
 
-with open( './data/store.txt', 'r', encoding = 'utf-8' ) as store:
+with open( './data/headlines.csv', 'r', encoding = 'utf-8' ) as store:
     while True:
         line = store.readline()
 
         if not line:
             break
 
-        try:
-            _, headline, _ = line.split( '|' )
-        except Exception as e:
-            print( line )
-            raise e
-
-        for c in headline:
+        for c in line:
             distribution[c] += 1
 
 sorted_dist = sorted( distribution.items(), key=itemgetter( 1 ) )
 
 with open( './data/distribution.txt', 'w', encoding = 'utf-8' ) as dist_file:
+    print( str(len(sorted_dist)) + " chars" )
+    
     for char, occurence in sorted_dist:
         dist_file.write( str(char) + ', ' + str(occurence) + '\n' )
-    dist_file.write( '"' + '", "'.join( sorted( [char for char, _ in sorted_dist] ) ) + '"\n' )
+
+    chars = sorted( [char for char, _ in sorted_dist] )
+
+    if '\n' in chars:
+        chars[chars.index('\n')] = "\\n"
+    if '"' in chars:
+        chars[chars.index('"')] = "\\\""
+
+    dist_file.write( '"' + '", "'.join( chars ) + '"\n' )
 
 plt.hist( [o for c, o in distribution.items()], label = [c for c, o in distribution.items()] )
 plt.show()
